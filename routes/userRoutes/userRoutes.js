1eb6482220b284be1ccc93b3bc51a62ec94891d2
Salman-Irfan <salmanirfan674@gmail.com>
2023-11-08 12:34:57 +0500

    auth middle ware

--- routes/userRoutes/userRoutes.js
+++ routes/userRoutes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const getAllUsersController = require('../../controllers/userControllers/getAllUsersController');
+const requireSignIn = require('../../middlewares/authMiddleware');
 const router = express.Router();
 
 // get all users route
-router.get('/get-all-users', getAllUsersController );
+router.get('/get-all-users', requireSignIn, getAllUsersController );
 
 
 module.exports = router;
