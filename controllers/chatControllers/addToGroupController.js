ec97e5ea69ff39fec5964840625f125791b5d70e
Salman-Irfan <salmanirfan674@gmail.com>
2023-11-11 15:48:36 +0500

    add and remove from group

--- controllers/chatControllers/addToGroupController.js
+++ controllers/chatControllers/addToGroupController.js
@@ -1,5 +1,37 @@
 const express = require('express')
-const addToGroupController = async (req, res) => {
+const Chat = require("../../models/chatModel")
 
+
+const addToGroupController = async (req, res) => {
+    const { chatId, userId } = req.body
+
+    try {
+        const added = await Chat.findByIdAndUpdate(
+            chatId, {
+            $push: { users: userId },
+
+        }, {
+            new: true
+        })
+            .populate("users", "-password")
+            .populate("groupAdmin", "-password")
+
+        if (!added) {
+            return res.json({
+                message: "user not added"
+            })
+        } else {
+            return res.json({
+                message: "user added successfully",
+                data: added
+            })
+        }
+    } catch (error) {
+        return res.json({
+            error: error
+        })
+    }
+
+    
 }
 module.exports = addToGroupController
--- controllers/chatControllers/removeFromGroupController.js
+++ controllers/chatControllers/removeFromGroupController.js
@@ -1,5 +1,35 @@
 const express = require('express');
-const removeFromGroupController = async () => {
+const Chat = require("../../models/chatModel")
 
+
+const removeFromGroupController = async (req, res) => {
+    const { chatId, userId } = req.body
+
+    try {
+        const removed = await Chat.findByIdAndUpdate(
+            chatId, {
+            $pull: { users: userId },
+
+        }, {
+            new: true
+        })
+            .populate("users", "-password")
+            .populate("groupAdmin", "-password")
+
+        if (!removed) {
+            return res.json({
+                message: "user not removed"
+            })
+        } else {
+            return res.json({
+                message: "user removed successfully",
+                data: removed
+            })
+        }
+    } catch (error) {
+        return res.json({
+            error: error
+        })
+    }
 }
 module.exports = removeFromGroupController
